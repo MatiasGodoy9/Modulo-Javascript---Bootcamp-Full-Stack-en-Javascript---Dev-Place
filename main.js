df3d25473e6ec747fb47f524bceae5399ec22d6c
Godoy Matias <pm_godoy@hotmail.com>
2022-10-25 14:14:39 -0300

    Funciones

--- main.js
+++ main.js
@@ -1,47 +1,55 @@
-// Primer consigna
+// PUNTO 1
 
-const iva = 10;
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
-const producto = 10000;
+meses.forEach(element => {
+    console.log(element)
+});
 
-let ivaProducto = producto * iva / 100;
+// PUNTO 2
 
-let compraFinal = producto + ivaProducto;
-
-console.log("El valor del producto final es: " + compraFinal);
-
-// Segunda consigna
-
-let resp = prompt("¿Cuantos creditos quiere comprar? (En caso de no querer ninguno ingrese 0");
-
-switch (resp) {
-    case '1':
-        console.log('Tiene acceso a Consolas');
-        break;
-    case '2':
-        console.log('Tiene acceso a Consolas y juegos 2D');
-        break;
-    case '3':
-        console.log('Tiene acceso a Consolas, juegos 2D y juegos 3D');
-        break;
-    case '4':
-        console.log('Tiene acceso a consolas, juegos 2D, juegos 3D y realidad virtual');
-        break;
-    case '0':
-        console.log('Chau');
-        break;
+function parImpar(numero) {
+    if ((numero % 2) == 0) {
+        return console.log("El numero es par")
+    } else {
+        return console.log("El numero es impar")
+    }
 }
 
-// Tercera consigna
+parImpar(10);
 
-/* const coche = prompt("¿Que tipo de coche desea? 1 - gasolina, 2 - diesel, 3 - electrico");
+// PUNTO 3
 
-if (coche == '1' || 'gasolina') {
-    console.log("dirigirse a la oficina 100")
-} else if (coche == '2' || 'diesel') {
-    console.log("Dirigirse a la oficina 200")
-} else if (coche == '3' || 'electrico') {
-    console.log("Dirigirse a la oficina 300")
-} else {
-    console.log("Respuesta incorrecta")
-} */
+let cadena = prompt ("Escribe algo con mayúsculas y minúsculas.");
+
+function numero(cadena){
+    let may=0;
+    let min=0;
+    let letra = cadena.split("");
+
+    for(let i = 0; i < cadena.length; i++){
+        if(letra[i]!= " "){
+            if(letra[i] == (letra[i].toUpperCase())) {
+                may++;    
+            }
+            if(letra[i] == letra[i].toLowerCase()) {
+                min++;
+            }
+        }    
+    }
+
+    console.log(cadena)
+    console.log("El numero de mayúsculas que hay: "+may);
+    console.log("El numero de minusculas que hay: "+min);
+}
+numero(cadena);
+
+// PUNTO 4
+
+let n = prompt("Introduce un número y se mostrará su factorial");
+let resultado = 1;
+
+for(let i=1; i<=n; i++) {
+  resultado *= i;
+}
+console.log(resultado);
